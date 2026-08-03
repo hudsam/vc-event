5ced70b52779aac5a945880f4211e526a9443944
hudsam <app.hoedhya@gmail.com>
2026-08-03 12:56:24 +0700

    added provider : appservice

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Auth\ApiUserProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Auth::provider('api-users', function ($app, array $config) {
+            return new ApiUserProvider();
+        });
     }
 }
